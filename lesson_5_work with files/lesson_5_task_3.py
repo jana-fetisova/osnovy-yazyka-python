426bca425dceed1fa8d193fe4802ba219f2fc45c
jana-fetisova <inffferna@mail.ru>
2022-01-09 13:13:09 +0300

    Added 7 files for hometask

---
+++ lesson_5_work with files/lesson_5_task_3.py
@@ -0,0 +1,4 @@
+with open('3.txt', 'r', encoding='utf-8') as f:
+    employees_dict = {line.split()[0]: float(line.split()[1]) for line in f}
+    print(f'Средняя зарплата = {round(sum(employees_dict.values()) / len(employees_dict), 3)}\n'
+          f'Сотрудники с зарплатой менее 20000: {[e[0] for e in employees_dict.items() if e[1] < 20000]}')
